394da416694e5f728d175fe1f3c054b93acfc8d6
Jhony Garcia <jhonyagarcia@gmail.com>
2020-04-20 06:33:54 -0500

    Práctica de encapsulamient, getters and setters en python

---
+++ encapsulacion.py
@@ -0,0 +1,19 @@
+
+
+class CasillaDeVotacion:
+    
+    def __init__(self, identificador, pais):
+        self._identificador = identificador
+        self._pais = pais
+        self._region = None
+    
+    @property #getter
+    def region(self):
+        return self._region
+
+    @region.setter #setter
+    def set_region(self, region):
+        if region in self._pais:
+            self._region = region
+        
+        raise ValueError(f'La region {region} no es valida en {self._pais}')
